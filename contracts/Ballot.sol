97c319a76ea594adb2236a6aa2e32e69c8a2f8fb
Adrien <280407@supinfo.com>
2020-03-19 14:32:54 +0100

    Change

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,5 +1,7 @@
-pragma solidity >=0.4.22 <0.7.0;
+pragma solidity >=0.4.20;
 pragma experimental ABIEncoderV2;
+
+import "./SafeMath.sol";
 
 /** 
  * @title Ballot
@@ -7,6 +9,8 @@ pragma experimental ABIEncoderV2;
  */
 contract Ballot {
    
+  using SafeMath for uint256;
+  
     struct Voter {
         uint weight; // weight is accumulated by delegation
         bool voted;  // if true, that person already voted
@@ -27,24 +31,24 @@ contract Ballot {
 
     Proposal[] public proposals;
     
-    uint32 private dateEnd;
+    uint32 public dateEnd;
 
     /** 
      * @dev Create a new ballot to choose one of 'proposalNames'.
-     * @param proposalNames names of proposals
+     * @param _proposalNames names of proposals
      */
-    constructor(string[] memory proposalNames, uint32 _dateEnd) public {
+    constructor(string[] memory _proposalNames, uint32 _dateEnd) public {
         chairperson = msg.sender;
-        voters[chairperson].weight = 1;
+        voters[chairperson].weight = voters[chairperson].weight.add(1);
         dateEnd = _dateEnd;
         
 
-        for (uint i = 0; i < proposalNames.length; i++) {
+        for (uint i = 0; i < _proposalNames.length; i++) {
             // 'Proposal({...})' creates a temporary
             // Proposal object and 'proposals.push(...)'
             // appends it to the end of 'proposals'.
             proposals.push(Proposal({
-                name: proposalNames[i],
+                name: _proposalNames[i],
                 voteCount: 0
             }));
         }
@@ -52,66 +56,66 @@ contract Ballot {
     
     /** 
      * @dev Give 'voter' the right to vote on this ballot. May only be called by 'chairperson'.
-     * @param voter address of voter
+     * @param _voter address of voter
      */
-    function giveRightToVote(address voter) public {
+    function giveRightToVote(address _voter) public {
         require(
             msg.sender == chairperson,
             "Only chairperson can give right to vote."
         );
         require(
-            !voters[voter].voted,
+            !voters[_voter].voted,
             "The voter already voted."
         );
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
+        require(voters[_voter].weight == 0);
+        voters[_voter].weight = voters[_voter].weight.add(1);
     }
 
     /**
      * @dev Delegate your vote to the voter 'to'.
-     * @param to address to which vote is delegated
+     * @param _to address to which vote is delegated
      */
-    function delegate(address to) public {
+    function delegate(address _to) public {
         Voter storage sender = voters[msg.sender];
         require(!sender.voted, "You already voted.");
-        require(to != msg.sender, "Self-delegation is disallowed.");
+        require(_to != msg.sender, "Self-delegation is disallowed.");
 
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
+        while (voters[_to].delegate != address(0)) {
+            _to = voters[_to].delegate;
 
             // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
+            require(_to != msg.sender, "Found loop in delegation.");
         }
         sender.voted = true;
-        sender.delegate = to;
-        Voter storage delegate_ = voters[to];
+        sender.delegate = _to;
+        Voter storage delegate_ = voters[_to];
         if (delegate_.voted) {
             // If the delegate already voted,
             // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
+            proposals[delegate_.vote].voteCount = proposals[delegate_.vote].voteCount.add(sender.weight);
         } else {
             // If the delegate did not vote yet,
             // add to her weight.
-            delegate_.weight += sender.weight;
+            delegate_.weight = delegate_.weight.add(sender.weight);
         }
     }
 
     /**
      * @dev Give your vote (including votes delegated to you) to proposal 'proposals[proposal].name'.
-     * @param proposal index of proposal in the proposals array
+     * @param _proposal index of proposal in the proposals array
      */
-    function vote(uint proposal) public {
+    function vote(uint _proposal) public {
         require(dateEnd >= now);
         Voter storage sender = voters[msg.sender];
         require(sender.weight != 0, "Has no right to vote");
         require(!sender.voted, "Already voted.");
         sender.voted = true;
-        sender.vote = proposal;
+        sender.vote = _proposal;
 
         // If 'proposal' is out of the range of the array,
         // this will throw automatically and revert all
         // changes.
-        proposals[proposal].voteCount += sender.weight;
+        proposals[_proposal].voteCount = proposals[_proposal].voteCount.add(sender.weight);
     }
 
     /** 
@@ -138,5 +142,15 @@ contract Ballot {
             returns (string memory winnerName_)
     {
         winnerName_ = proposals[winningProposal()].name;
+    }
+    
+    /**
+     * @dev Calls alreadyVote() function verify if a voter as already voter
+     * @return alreadyVote_
+     */
+    function alreadyVote() public view returns(bool alreadyVote_)
+    {
+        Voter storage sender = voters[msg.sender];
+        alreadyVote_ = sender.voted;
     }
 }
